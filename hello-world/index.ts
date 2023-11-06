4cbd3bcdf84ec415b4eb6c1f9fc3cce84cc1f8b9
Felix Palmer <felixpalmer@gmail.com>
2023-11-06 10:49:48 +0100

    Fix hello world

--- hello-world/index.ts
+++ hello-world/index.ts
@@ -1,19 +1,15 @@
 import './style.css';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import { Deck } from '@deck.gl/core';
-import {
-  BASEMAP,
-  vectorTableSource,
-  VectorTileLayer
-} from '@deck.gl/carto';
+import {Deck} from '@deck.gl/core';
+import {BASEMAP, vectorTableSource, VectorTileLayer} from '@deck.gl/carto';
 
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 const accessToken = import.meta.env.VITE_API_ACCESS_TOKEN;
 const connectionName = 'carto_dw';
-const cartoConfig = { apiBaseUrl, accessToken, connectionName };
+const cartoConfig = {apiBaseUrl, accessToken, connectionName};
 
-console.log(cartoConfig)
+console.log(cartoConfig);
 
 const route = (document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div id="map"></div>
@@ -25,12 +21,12 @@ const INITIAL_VIEW_STATE = {
   longitude: -98.5556199,
   zoom: 4,
   bearing: 0,
-  pitch: 30,
+  pitch: 30
 };
 
-const vectorTableSource = vectorTableSource({
+const data = vectorTableSource({
   ...cartoConfig,
-  tableName: 'carto-demo-data.demo_tables.populated_places',
+  tableName: 'carto-demo-data.demo_tables.populated_places'
 });
 
 const deck = new Deck({
@@ -40,9 +36,9 @@ const deck = new Deck({
   layers: [
     new VectorTileLayer({
       id: 'places',
-      data: vectorTableSource,
+      data,
       pointRadiusMinPixels: 3,
-      getFillColor: [200, 0, 80],
+      getFillColor: [200, 0, 80]
     })
   ]
 });
@@ -51,12 +47,11 @@ const deck = new Deck({
 const map = new maplibregl.Map({
   container: 'map',
   style: BASEMAP.VOYAGER,
-  interactive: false,
+  interactive: false
 });
 deck.setProps({
-  onViewStateChange: ({ viewState }) => {
-    const { longitude, latitude, ...rest } = viewState;
-    map.jumpTo({ center: [longitude, latitude], ...rest });
-  },
+  onViewStateChange: ({viewState}) => {
+    const {longitude, latitude, ...rest} = viewState;
+    map.jumpTo({center: [longitude, latitude], ...rest});
+  }
 });
-
