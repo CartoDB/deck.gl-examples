92e0fcef4501f66a852fe675a2c9eca921c343dd
menusal <daniarlandis@gmail.com>
2023-11-14 18:12:24 +0100

    Rename example

--- labels/index.ts
+++ labels/index.ts
@@ -1,14 +1,15 @@
 import './style.css';
-import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import {Deck} from '@deck.gl/core';
+import maplibregl from 'maplibre-gl';
 import {vectorTableSource, VectorTileLayer} from '@deck.gl/carto';
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
-const accessToken = import.meta.env.VITE_API_ACCESS_TOKEN;
-const connectionName = 'carto_dw';
-const cartoConfig = {apiBaseUrl, accessToken, connectionName};
 import {CollisionFilterExtension} from '@deck.gl/extensions';
 import {scaleLinear} from 'd3-scale';
+
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+const accessToken = import.meta.env.VITE_API_ACCESS_TOKEN;
+const connectionName = import.meta.env.VITE_API_CONNECTION_NAME;
+const cartoConfig = {apiBaseUrl, accessToken, connectionName};
 
 const INITIAL_VIEW_STATE = {
   latitude: 39.1,
