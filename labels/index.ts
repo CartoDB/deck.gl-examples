dae76a7853a65f9a2f5f027b6dd3c1536019500d
menusal <daniarlandis@gmail.com>
2023-11-21 11:03:09 +0100

    remove connection name

--- labels/index.ts
+++ labels/index.ts
@@ -8,7 +8,7 @@ import {scaleLinear} from 'd3-scale';
 
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 const accessToken = import.meta.env.VITE_API_ACCESS_TOKEN;
-const connectionName = import.meta.env.VITE_API_CONNECTION_NAME;
+const connectionName = 'carto_dw';
 const cartoConfig = {apiBaseUrl, accessToken, connectionName};
 
 const INITIAL_VIEW_STATE = {
