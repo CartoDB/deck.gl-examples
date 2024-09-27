bcb7b4a6ee24509cd8ef0af1eddf11d9b9e1a37f
Álex Tena <srtena@gmail.com>
2024-09-27 19:39:39 +0200

    Adds clear filter + uses utils function for viewport

--- hello-world-with-widgets/index.ts
+++ hello-world-with-widgets/index.ts
@@ -1,11 +1,18 @@
 import './style.css';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import {Deck} from '@deck.gl/core';
-import {vectorTableSource, addFilter, clearFilters} from '@carto/api-client';
+import {WebMercatorViewport, Deck} from '@deck.gl/core';
+import {
+  vectorTableSource,
+  addFilter,
+  clearFilters,
+  removeFilter,
+  FilterType,
+  createViewportSpatialFilter
+} from '@carto/api-client';
 import {BASEMAP, VectorTileLayer} from '@deck.gl/carto';
 import * as echarts from 'echarts';
-import {debounce, createViewStatePolygon} from './utils';
+import {debounce} from './utils';
 
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 const accessToken = import.meta.env.VITE_API_ACCESS_TOKEN;
@@ -17,8 +24,7 @@ const cartoConfig = {apiBaseUrl, accessToken, connectionName};
 const INITIAL_VIEW_STATE = {
   latitude: 41.8097343,
   longitude: -110.5556199,
-  zoom: 5,
-  minZoom: 3, // TODO: remove once polygon validation is done
+  zoom: 1,
   bearing: 0,
   pitch: 0
 };
@@ -37,25 +43,73 @@ const map = new maplibregl.Map({
   interactive: false
 });
 
+// prepare and init widgets HTML elements
+
+const formulaWidget = document.querySelector<HTMLSelectElement>('#widget1');
+
+const histogramWidget = document.querySelector<HTMLSelectElement>('#widget2');
+
+var chartDom = histogramWidget;
+var histogramWidgetChart = echarts.init(chartDom);
+
+// add click filter interaction
+
+histogramWidgetChart.on('click', function (params) {
+  if (params.componentType === 'series') {
+    filterViaHistogram(params.dataIndex);
+  }
+});
+
+// define source
+
+let dataSource;
+
+async function initSource() {
+  return (dataSource = await vectorTableSource({
+    ...cartoConfig,
+    tableName: 'carto-demo-data.demo_tables.populated_places',
+    filters: filters
+  }));
+}
+
+// dictionary for histogram labels
+
+const histogramLabels = {
+  0: '0 - 50k',
+  50000: '50k - 100k',
+  100000: '100k - 150k',
+  150000: '150k - 200k',
+  200000: '200k - 250k',
+  250000: '250k - 300k',
+  300000: '300k - 350k',
+  350000: '350k - 400k',
+  400000: '400k - 450k',
+  450000: '450k - 500k',
+  500000: '> 500k'
+};
+
+// SPATIAL FILTER
 // prepare a function to get the new viewport state, that we'll pass debounced to our widgets to minimize requests
 
 const debouncedRenderWidgets = debounce(renderWidgets, 500);
 
 // sync deckgl map after user interaction, obtain new viewport after
 
-let currentViewStatePolygon;
+let viewportSpatialFilter;
 
 deck.setProps({
   onViewStateChange: ({viewState}) => {
     const {longitude, latitude, ...rest} = viewState;
     map.jumpTo({center: [longitude, latitude], ...rest});
-    currentViewStatePolygon = createViewStatePolygon(viewState);
+    const viewport = new WebMercatorViewport(viewState);
+    viewportSpatialFilter = createViewportSpatialFilter(viewport.getBounds());
     formulaWidget.innerHTML = 'Loading...';
     histogramWidgetChart.showLoading();
     debouncedRenderWidgets();
   }
 });
 
+// COLUMN FILTERS
 // prepare filters
 
 let filters = {};
@@ -72,53 +126,63 @@ countrySelector?.addEventListener('change', () => {
   } else {
     addFilter(filters, {
       column: 'sov_a3',
-      type: 'in',
+      type: FilterType.IN,
       values: [country]
     });
   }
   selectedCountry = countrySelector.value;
-  formulaWidget.innerHTML = 'Loading...';
-  histogramWidgetChart.showLoading();
   initialize();
 });
 
-// prepare and init widgets HTML elements
+// filtering from the widget, including a clear filters button
 
-const formulaWidget = document.querySelector<HTMLSelectElement>('#widget1');
+const clearFiltersButton = document.querySelector<HTMLElement>('#clearHistogramFilter');
 
-const histogramWidget = document.querySelector<HTMLSelectElement>('#widget2');
+clearFiltersButton?.addEventListener('click', () => {
+  clearHistogramFilter();
+});
 
-var chartDom = histogramWidget;
-var histogramWidgetChart = echarts.init(chartDom);
+function filterViaHistogram(dataIndex) {
+  clearFiltersButton.style.display = 'inherit';
 
-// define source
+  const histogramKeys = Object.keys(histogramLabels).map(Number);
+  const minValue = histogramKeys[dataIndex];
+  const maxValue = histogramKeys[dataIndex + 1];
 
-let dataSource;
+  removeFilter(filters, {
+    column: 'pop_max'
+  });
 
-async function initSource() {
-  return (dataSource = await vectorTableSource({
-    ...cartoConfig,
-    tableName: 'carto-demo-data.demo_tables.populated_places',
-    filters: filters
-  }));
+  if (dataIndex === histogramKeys.length - 1) {
+    // For the last category (> 1M), use CLOSED_OPEN
+    addFilter(filters, {
+      column: 'pop_max',
+      type: FilterType.CLOSED_OPEN,
+      values: [[minValue, Infinity]]
+    });
+  } else {
+    // For first and middle categories, use BETWEEN
+    addFilter(filters, {
+      column: 'pop_max',
+      type: FilterType.BETWEEN,
+      values: [[minValue, maxValue]]
+    });
+  }
+
+  initialize();
 }
 
-// dictionary for histogram labels
+function clearHistogramFilter() {
+  clearFiltersButton.style.display = 'none';
 
-const histogramLabels = [
-  '< 100k',
-  '100k - 200k',
-  '200k - 300k',
-  '300k - 400k',
-  '400k - 500k',
-  '500k - 600k',
-  '600k - 700k',
-  '700k - 800k',
-  '800k - 900k',
-  '900k - 1M',
-  '> 1M'
-];
+  removeFilter(filters, {
+    column: 'pop_max'
+  });
 
+  initialize();
+}
+
+// RENDER
 // render Widgets function
 
 async function renderWidgets() {
@@ -126,14 +190,14 @@ async function renderWidgets() {
 
   const formula = await dataSource.widgetSource.getFormula({
     operation: 'count',
-    spatialFilter: currentViewStatePolygon
+    spatialFilter: viewportSpatialFilter
   });
 
   const histogram = await dataSource.widgetSource.getHistogram({
     column: 'pop_max',
-    ticks: [100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000],
+    ticks: [50000, 100000, 150000, 200000, 250000, 300000, 350000, 400000, 450000, 500000],
     operation: 'count',
-    spatialFilter: currentViewStatePolygon
+    spatialFilter: viewportSpatialFilter
   });
 
   const option = {
@@ -144,7 +208,7 @@ async function renderWidgets() {
       }
     },
     grid: {
-      left: 30,
+      left: 40,
       right: 30,
       top: 10,
       width: 'auto',
@@ -152,7 +216,7 @@ async function renderWidgets() {
     },
     xAxis: {
       type: 'category',
-      data: histogramLabels,
+      data: Object.values(histogramLabels),
       axisLabel: {
         rotate: 45, // Rotate the labels if they're too long
         interval: 0 // Show all labels
@@ -177,6 +241,7 @@ async function renderWidgets() {
 
   formulaWidget.innerHTML = formula.value;
   option && histogramWidgetChart.setOption(option);
+
   histogramWidgetChart.hideLoading();
 }
 
@@ -212,6 +277,8 @@ async function renderLayers() {
 // render everything!
 
 async function initialize() {
+  formulaWidget.innerHTML = 'Loading...';
+  histogramWidgetChart.showLoading();
   await initSource();
   renderWidgets();
   renderLayers();
