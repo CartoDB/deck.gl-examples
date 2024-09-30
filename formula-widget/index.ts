ae124bdf3eda14c860f5cf48245ea828a224f807
Álex Tena <srtena@gmail.com>
2024-09-30 12:34:57 +0200

    Adds basic formula widget example

---
+++ formula-widget/index.ts
@@ -0,0 +1,123 @@
+import './style.css';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import {WebMercatorViewport, Deck} from '@deck.gl/core';
+import {vectorTableSource, createViewportSpatialFilter} from '@carto/api-client';
+import {BASEMAP, VectorTileLayer} from '@deck.gl/carto';
+import {debounce} from './utils';
+
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+const accessToken = import.meta.env.VITE_API_ACCESS_TOKEN;
+const connectionName = 'carto_dw';
+const cartoConfig = {apiBaseUrl, accessToken, connectionName};
+
+// init deckgl
+
+const INITIAL_VIEW_STATE = {
+  latitude: 41.8097343,
+  longitude: -110.5556199,
+  zoom: 3,
+  bearing: 0,
+  pitch: 0
+};
+
+const deck = new Deck({
+  canvas: 'deck-canvas',
+  initialViewState: INITIAL_VIEW_STATE,
+  controller: true
+});
+
+// Add basemap
+
+const map = new maplibregl.Map({
+  container: 'map',
+  style: BASEMAP.POSITRON,
+  interactive: false
+});
+
+// prepare and init widgets HTML elements
+
+const formulaWidget = document.querySelector<HTMLSelectElement>('#formula-widget');
+
+// define source
+
+let dataSource;
+
+async function initSource() {
+  return (dataSource = await vectorTableSource({
+    ...cartoConfig,
+    tableName: 'carto-demo-data.demo_tables.osm_pois_usa'
+  }));
+}
+
+// SPATIAL FILTER
+// prepare a function to get the new viewport state, that we'll pass debounced to our widgets to minimize requests
+
+const debouncedRenderWidgets = debounce(renderWidgets, 500);
+
+// sync deckgl map after user interaction, obtain new viewport after
+
+let viewportSpatialFilter;
+
+deck.setProps({
+  onViewStateChange: ({viewState}) => {
+    const {longitude, latitude, ...rest} = viewState;
+    map.jumpTo({center: [longitude, latitude], ...rest});
+    const viewport = new WebMercatorViewport(viewState);
+    viewportSpatialFilter = createViewportSpatialFilter(viewport.getBounds());
+    formulaWidget.innerHTML = 'Loading...';
+    debouncedRenderWidgets();
+  }
+});
+
+// RENDER
+// render Widgets function
+
+async function renderWidgets() {
+  // configure widgets
+
+  const formula = await dataSource.widgetSource.getFormula({
+    operation: 'count',
+    spatialFilter: viewportSpatialFilter
+  });
+
+  // render widgets!
+
+  formulaWidget.innerHTML = formula.value;
+}
+
+// render Layers function
+
+async function renderLayers() {
+  // now for the layers
+
+  const layers = [
+    new VectorTileLayer({
+      id: 'places',
+      pickable: true,
+      data: dataSource,
+      opacity: 1,
+      getFillColor: [3, 111, 226],
+      getLineColor: [255, 255, 255],
+      getPointRadius: 50,
+      getLineWidth: 10,
+      pointRadiusMinPixels: 1,
+      lineWidthMinPixels: 0.3
+    })
+  ];
+
+  deck.setProps({
+    layers: layers
+  });
+}
+
+// render everything!
+
+async function initialize() {
+  formulaWidget.innerHTML = 'Loading...';
+  await initSource();
+  renderWidgets();
+  renderLayers();
+}
+
+initialize();
